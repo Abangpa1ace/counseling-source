8a0a9b462f70ba303027cedbfcff470ce79e808b
Abangpa1ace <dyek72731@gmail.com>
2025-08-27 01:55:49 +0900

    기본 페이지들 작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import Wrapper from "@/components/layout/Wrapper";
-import Image from "next/image";
+import MainBanner from "@/components/layout/MainBanner";
 
 export default function Home() {
   return (
     <>
-      <Image
-        src="/images/main-banner.png"
-        alt="main-banner"
-        width={1280}
-        height={600}
-        className="h-[600px] w-full object-cover"
-      />
+      <MainBanner />
       <Wrapper>
         <h1 className="text-[48px] font-bold">
           안녕하세요
